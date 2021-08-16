d962dd862158fe3f8c3af48c0efa8436d645d71c
김서영 <ksy990628@gmail.com>
2021-08-16 18:23:23 +0900

    medicine input
    axios post 구현 완료, 현 1개의 약품에 대한 정보만 입력 가능

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -4,12 +4,7 @@ import './index.css';
 function App() {
   return (
     <div>
-    <div className="header"></div>
-    <div className="body">
-      <div className="user_input">
-      <input placeholder="Medicine ID를 입력하세요."></input>
-      </div>
-    </div>
+      <Main/>
     </div>
   );
 }
--- healthcare/src/Main.js
+++ healthcare/src/Main.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './index.css';
 
 const Main = () => {
+    const [id,setId] = useState('');
+    const [name,setName] = useState('');
+    const [num,setNum] = useState(0);
+    const onChangeName = (e) => {
+        setName(e.target.value);
+    }
+    const onChangeId = (e) => {
+        setId(e.target.value);
+    }
+    const onChangeNum = (e) => {
+        setNum(e.target.value);
+    }    
+    const onClick = () => {
+        const data = {"medicine_id": id, "medicine_name": name, "num":num}
+        axios.post("http://localhost:5000/api/sendintake",data).then(
+            response=>{
+                console.log(data);
+            }
+        )
+    }
     return(
         <div>
-            <input>Medicine Id를 입력해주세요.</input>
+            <div className="header">
+                <h1>건강기능식품 프로젝트</h1>
+            </div>
+            <div className="body">
+            <input value={name} placeholder="약품 이름을 입력하세요." onChange={onChangeName}></input>
+            <input type="number" value={id} placeholder="약품 ID를 입력하세요." onChange={onChangeId}></input>
+            <input type="number" value={num} placeholder="섭취량을 입력하세요." onChange={onChangeNum}></input>
+            <button onClick={onClick}>성분 검색</button><br/>
+            </div>
         </div>
     );
 }
