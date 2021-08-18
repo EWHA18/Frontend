dadf13595a692175ca045df170e9963f36abdafd
김서영 <ksy990628@gmail.com>
2021-08-18 20:28:59 +0900

    list 추가

--- healthcare/src/Main.js
+++ healthcare/src/Main.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './index.css';
 const Main = () => {
-    const [id,setId] = useState('');
+    const [id,setId] = useState(0);
     const [name,setName] = useState('');
     const [num,setNum] = useState(0);
     const [intake,setIntake] = useState([]);
+    const [list,setList] = useState([]);
+    
     const onChangeName = (e) => {
         setName(e.target.value);
     }
@@ -16,12 +18,12 @@ const Main = () => {
         setNum(e.target.value);
     }    
     const onClick = async() => {
-        const data = {"medicine_id": id, "medicine_name": name, "num":num}
-        await axios.post("https://64015d57-ae66-4061-9d92-c81956d3738e.mock.pstmn.io/test2",data).then(
+        const data = {"medicine_id": id,"medicine_name": name, "num":num};
+        setList(list => [...list,data]);
+        await axios.post("https://da12cbf1-ea38-4684-8c59-5cec89c3f728.mock.pstmn.io/input",list).then(
             response=>{
-                console.log(data);
-            }
-        )
+                console.log(list);        
+            })
         const answer = await axios.get("https://64015d57-ae66-4061-9d92-c81956d3738e.mock.pstmn.io/test");
         setIntake(answer.data.data.intake);
     }
@@ -31,18 +33,23 @@ const Main = () => {
                 <h1>건강기능식품 프로젝트</h1>
             </div>
             <div className="body">
+            <div className="input">
             <input value={name} placeholder="약품 이름을 입력하세요." onChange={onChangeName}></input>
             <input type="number" value={id} placeholder="약품 ID를 입력하세요." onChange={onChangeId}></input>
             <input type="number" value={num} placeholder="섭취량을 입력하세요." onChange={onChangeNum}></input>
             <button onClick={onClick}>성분 검색</button><br/>
+            </div>
+            <div className="output">
             {intake.map(intake_element => (
-                <div>
+                <div key={intake_element.word_id}>
                     <li>{intake_element.word_id}번 {intake_element.volume}{intake_element.unit}</li>
                 </div>
             ))}
+            </div>
             </div>
         </div>
     );
 }
 
 export default Main;
+    // id, num e.target.value으로 바꾸기
