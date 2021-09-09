750c822e3bce69723eddd72cfeb0dbadadd572fc
betty214 <bettyjang@ewhain.net>
2021-09-09 17:34:48 +0900

    feat(중금속 버튼):filtering을 통한 중금속 버튼 구현

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -8,8 +8,11 @@ import axios from 'axios';
 function App() {
   const [medicines,setMedicines] = useState([]); 
   const nextId = useRef(1);
+  const [heavy,setHeavy] = useState([]);
   const [output,setOutput] = useState([]);
-
+  const [test,setTest] = useState([]);
+  const [checked,setCheck] = useState([]);
+  const [show_state,setShow] = useState([]);
   const onInsert = useCallback(
     data => {
       const medicine = {
@@ -18,15 +21,31 @@ function App() {
         intake: data.get('intake')
   };
   setMedicines(medicines.concat(medicine));
+
   nextId.current += 1;
 },[medicines]);
 
 const onClick = async() => {
-  console.log(medicines);
   await axios.post("http://localhost:5000/api/sendintake",medicines).then(
       async response=>{
-        setOutput(response.data.data.intake);       
+        console.log(response.data.data.intake);
+        setHeavy(response.data.data.intake.filter(intake_element => intake_element.isHeavyMetal==true));       
+        setOutput(response.data.data.intake); 
+        setTest(response.data.data.intake);
+        setCheck(false);
+        setShow("중금속만 보기");
       })
+}
+const heavy_button = () => {
+  if(!checked){
+    setTest(heavy);
+    setCheck(true);
+    setShow("전체 보기");
+  }else{
+    setTest(output);
+    setCheck(false);
+    setShow("중금속만 보기");
+  }
 }
   return (
     <div>
@@ -38,17 +57,16 @@ const onClick = async() => {
           <InputMedicine onInsert={onInsert}/>
         </div>
         <button className="sendBtn" onClick={onClick}>총 성분 섭취량 구하기</button>
-        
+        <button className="heavyBtn" onClick={heavy_button}>{show_state}</button>
         <div className="input">
           <h3>Input</h3>
           <InputMedicineList medicines={medicines}/>
         </div>
-        
         <div className="output">
           <h3>Output</h3>
-        {output.map(intake_element => (
+          {test.map(intake_element => (
                 <div key={intake_element.word_id} className="medicine-">
-                    <li>{intake_element.word_id}번 {intake_element.volume}{intake_element.unit}</li>
+                    <li>{intake_element.word_name} {intake_element.volume}{intake_element.unit}</li>
                 </div>
             ))}
         </div> 
