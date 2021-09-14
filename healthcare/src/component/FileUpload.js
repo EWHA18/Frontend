31708a30506f290e05da5d1891e61900d71af9b4
betty214 <bettyjang@ewhain.net>
2021-09-14 16:05:06 +0900

    feat:페이지 분리 및 중금속 버튼 csv파일 부분에도 추가

--- healthcare/src/component/FileUpload.js
+++ healthcare/src/component/FileUpload.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
-import React, { Component, useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
+import '../index.css';
 
 const FileUpload = () => {
 	const [selectedFile, setSelectedFile] = useState(null);
-	const [data,setData] = useState([]);
-	const [name,setName] = useState([]);
-	const [index,setIndex] = useState([]);
+	const [data, setData] = useState([]);
+	const [heavy, setHeavy] = useState([]);
+	const [total, setTotal] = useState([]);
+	const [name, setName] = useState([]);
+	const [checked,setCheck] = useState([]);
+	const [index, setIndex] = useState([]);
+	const [show_state,setShow] = useState([]);
 	const onFileChange = e => {
 		setSelectedFile(e.target.files[0]);
 	};
@@ -15,16 +20,22 @@ const FileUpload = () => {
 		console.log(selectedFile);
 		axios.post("http://localhost:5000/api/sendfile", formData).then(
 			async response => {
-				let temp_name=[];
-				let temp_data=[];
-				let temp_index=[];
+				let temp_name = [];
+				let temp_data = [];
+				let temp_index = [];
+				let temp_heavy=[];
 				for (let i = 0; i < response.data.data.length; i++) {
 					temp_name.push(response.data.data[i].name);
 					temp_data.push(response.data.data[i].intake);
 					temp_index.push(i);
-				}setName(temp_name);
+					temp_heavy.push(response.data.data[i].intake.filter(f=>f.isHeavyMetal==true));
+				} setName(temp_name);
 				setData(temp_data);
+				setTotal(temp_data);
 				setIndex(temp_index);
+				setHeavy(temp_heavy);
+				setCheck(false);
+				setShow("중금속만 보기");
 			});
 	};
 
@@ -41,27 +52,43 @@ const FileUpload = () => {
 			);
 		}
 	};
+	const heavy_button = () => {
+		if(!checked){
+		  setTotal(heavy);
+		  setCheck(true);
+		  setShow("전체 보기");
+		}else{
+		  setTotal(data);
+		  setCheck(false);
+		  setShow("중금속만 보기");
+		}
+	  }
 	return (
 		<div>
-			<h3>사용자의 약품 내역을 파일로 업로드해보세요!</h3><br />
-			<div>
-				<input type="file" onChange={onFileChange} />
-				<button onClick={onFileUpload}>
-					파일 업로드
-				</button>
+			<div className="header">
+				<h1>건강기능식품 프로젝트</h1>
 			</div>
-			{fileData()}
-			{index.map(i=>(
+			<div className="input">
+				<h3>사용자의 약품 내역을 파일로 업로드해보세요!</h3><br />
 				<div>
-				{name[i]}
-				{data[i].map(t=><li>{t.word_name} {t.volume}{t.unit}</li>)}
+					<input type="file" onChange={onFileChange} />
+					<button onClick={onFileUpload}>
+						파일 업로드
+					</button>
 				</div>
-			))}
+				{fileData()}
+			</div>
+			<div className="output">
+			{index.map(i => (
+					<div>
+						{name[i]}
+						{total[i].map(t => <li>{t.word_name} {t.volume}{t.unit}</li>)}
+						<br/>
+					</div>
+				))}
+			</div>
+			<button className="heavyBtn" onClick={heavy_button}>{show_state}</button>
 		</div>
 	);
 }
-/*
-				{name[0]}
-				{test[0].map(t=><li>{t.word_name} {t.volume}{t.unit}</li>)}
-				*/
 export default FileUpload;
